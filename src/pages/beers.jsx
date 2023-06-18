9d06cbb9b6cd262cd69f757f3907595133013a51
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-06-18 23:00:54 +0200

    Iteration 3

--- src/pages/beers.jsx
+++ src/pages/beers.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-function beers() {
+function Beers() {
+  const [data, setData] = useState([]);
+
+  const url = "https://ih-beers-api2.herokuapp.com/beers";
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  console.log(data);
+
   return (
-    <div>beers</div>
-  )
+    <div>
+      {data.map((beer) => {
+        return (
+          <div>
+            <div className="p-5 flex ">
+              <div>
+                <Link to={`/beers/${beer._id}`}>
+                  <img className="max-h-24" src={beer.image_url} alt={beer.name} />
+                </Link>
+              </div>
+              <div className="flex flex-col pl-5">
+                <Link to={`/beers/${beer._id}`}>
+                  <h2>{beer.name}</h2>
+                  <h3 className="text-gray-500">{beer.tagline}</h3>
+                  <p className="pb-5 text-xs text-gray-400">Created by: {beer.contributed_by}</p>
+                </Link>
+              </div>
+            </div>
+            <hr className="p-5"/>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default beers
+export default Beers;
