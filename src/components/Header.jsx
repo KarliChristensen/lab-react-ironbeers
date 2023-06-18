323a5bd33f40ec6c855eaa421476c30cbeeda8d6
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-06-18 21:51:05 +0200

    Iteration 2 - Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import houseIcon from "../assets/Daco_4812335.png";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="w-[100%] h-24 bg-blue-400 flex justify-center align">
-      <Link className="text-white p-3 font-bold" to="/">Home</Link>
-      <Link className="text-white p-3 font-bold" to="/beers">Beers</Link>
-      <Link className="text-white p-3 font-bold" to="/new-beer">Random</Link>
-      <Link className="text-white p-3 font-bold" to="/random-beer">Create-a-beer</Link>
+    <div className="w-[100%] h-24 bg-blue-400 flex justify-center align flex-col">
+      <div className="flex justify-center">
+        <Link className="text-white p-3 font-bold" to="/">
+          <img src={houseIcon} alt="home icon" className="h-10" />
+        </Link>
+      </div>
+      <div>
+        <Link className="text-white p-3 font-bold" to="/beers">
+          Beers
+        </Link>
+        <Link className="text-white p-3 font-bold" to="/new-beer">
+          Random
+        </Link>
+        <Link className="text-white p-3 font-bold" to="/random-beer">
+          Create-a-beer
+        </Link>
+      </div>
     </div>
   );
 }
